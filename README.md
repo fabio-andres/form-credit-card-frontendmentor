00cfbd0f5197bbc799fa790ec759045d03587ddb
fabio_andres <fabioandresing@gmail.com>
2022-08-24 23:39:07 -0500

    dd

---
+++ README.md
@@ -0,0 +1,11 @@
+# rating card
+
+this is a form for credit card. this is a challenge solved from www.frontenmentor.io
+
+## Preview
+
+![](https://github.com/fabio-andres/form-credit-card/blob/master/Captura%20de%20pantalla%202022-08-24%20232050.png)
+
+## Getting Started 🚀
+
+[click here to see the project](https://fabio-andres.github.io/rating-card/)
---
+++ main.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("form")
+const names = document.querySelector(".card-front-text2")
+const num = document.querySelector(".card-front-text1")
+const date = document.querySelector(".card-front-text3")
+const cvc = document.querySelector(".card-back-text")
+const section2 = document.querySelector(".sec2")
+
+section2.classList.add("hide")
+
+form.addEventListener('submit', sendForm)
+
+function sendForm(e) {
+    e.preventDefault();
+    const data = new FormData(e.target);
+
+    names.textContent = data.get("name")
+    num.textContent = data.get("num")
+    cvc.textContent = data.get("cvc")
+    date.textContent = `${data.get("month")}/${data.get("year")}`
+
+    section2.classList.remove("hide")
+
+    form.classList.add("hide")
+
+}
